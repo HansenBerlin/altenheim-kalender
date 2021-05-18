352a2bcde1e75a3539321684e7d7e9e9271852d4
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2021-05-18 19:44:22 +0200

    Add: mögliche Termine anhand von den Parametern (erster Termin, Intervall, Streuung) finden

--- src/controller/AppointmentSuggestionController.java
+++ src/controller/AppointmentSuggestionController.java
@@ -1,6 +1,9 @@
 ﻿package controller;
 
 import java.util.Calendar;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import interfaces.IAppointmentEntryFactory;
 import interfaces.IAppointmentSuggestionController;
 import interfaces.ICalendarEntriesModel;
@@ -10,7 +13,7 @@ public class AppointmentSuggestionController implements IAppointmentSuggestionCo
 {
     private ICalendarEntriesModel allEntries;
     private IAppointmentEntryFactory administrateEntries;
-
+    
     public AppointmentSuggestionController(ICalendarEntriesModel allEntries, IAppointmentEntryFactory administrateEntries)
     {
         this.allEntries = allEntries;
@@ -23,13 +26,67 @@ public class AppointmentSuggestionController implements IAppointmentSuggestionCo
 
     public int testFunction()
     {
-        int days = 0;
-        for (ICalendarEntryModel entry : allEntries.getAllRandomDates()) 
-        {
-            if (entry != null)
-                days += entry.getDate(true).get(Calendar.DAY_OF_YEAR);            
-        }
-        return days;
+//        int days = 0;
+//        for (ICalendarEntryModel entry : allEntries.getAllRandomDates()) 
+//        {
+//            if (entry != null)
+//            {
+//                days += entry.getDate(true).get(Calendar.DAY_OF_YEAR); 
+//            	System.out.println(entry.getDate(true).get(Calendar.DAY_OF_YEAR));
+//            }
+//        }
+        
+//        return days;
+    	boolean[] tage = new boolean[365];
+    	int erster_Termin = 23;
+    	int intervall = 60;
+    	int streuung = 0;
+    	int max_Terminevorschläge = 3;
+    	for (int i = 0; i < tage.length; i++) 
+    	{
+			if (Math.random()<= 0.5) 
+			{
+				tage[i]= true;
+			}
+			else 
+			{
+				tage[i]=false;
+				
+			}
+		}
+    	LinkedList<Integer> mögliche_termine = new LinkedList<Integer>();
+    	if (tage[erster_Termin+intervall] && erster_Termin+intervall%7!=0) {
+			mögliche_termine.add(erster_Termin+intervall);
+		}
+    	for (int i = 1; i < streuung; i++) 
+    	{
+			if (tage[erster_Termin+intervall-i] && (erster_Termin+intervall-i)%7!=0) {
+				mögliche_termine.add(erster_Termin+intervall-i);
+			}
+			if (tage[erster_Termin+intervall+i] && (erster_Termin+intervall+i)%7!=0) {
+				mögliche_termine.add(erster_Termin+intervall+i);
+			}
+			if (mögliche_termine.size()>=max_Terminevorschläge) {
+				break;
+			}
+		}
+    	if (mögliche_termine.size()==0) 
+    	{
+			System.out.println("Es gibt keine Termine, die in das Streufeld fallen.");
+		}
+    	for (int i = 0; i < mögliche_termine.size(); i++) {
+			System.out.println("Der Tag: "+mögliche_termine.get(i)+" ist frei: "+tage[mögliche_termine.get(i)]);
+		}
+    	
+    	
+    	if (mögliche_termine.get(0)!=null) 
+    	{
+    		return mögliche_termine.get(0);
+		}
+    	else 
+		{
+    		return 0;
+		}
     }
 
     public void testFunctionTwo()
