ba892ba42a8e3f23c64305202cd01f3a6d84f369
LT-6FGD373\Hannes <h.roever@zertificon.com>
2021-05-21 01:07:57 +0200

    fix: missing constructor parameter

--- src/controller/AppointmentEntryFactory.java
+++ src/controller/AppointmentEntryFactory.java
@@ -89,6 +89,7 @@ public class AppointmentEntryFactory implements IAppointmentEntryFactory
             return null;
         var startAppointment = new GregorianCalendar(startDate[0], startDate[1]-1, startDate[2], startTime[0], startTime[1]);
         var endAppointment = new GregorianCalendar(endDate[0], endDate[1]-1, endDate[2], endTime[0], endTime[1]);
+       
         ICalendarEntryModel newEntry = new CalendarEntryModel(travelTime);
         newEntry.resetDates(startAppointment, endAppointment);
         newEntry.resetAppointmentEntryName(entryName);
