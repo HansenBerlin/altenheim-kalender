0bb80f5ffab39000ef1832d3009fa58b3ab5b3f4
LT-6FGD373\Hannes <h.roever@zertificon.com>
2021-05-23 15:21:14 +0200

    test break unit test

--- src/test/java/com/altenheim/tests/AppointmentEntryFactoryTest.java
+++ src/test/java/com/altenheim/tests/AppointmentEntryFactoryTest.java
@@ -36,6 +36,12 @@ public class AppointmentEntryFactoryTest
     }
 
     @Test
+    void testFail()
+    {
+        Assertions.assertTrue(false);
+    }
+
+    @Test
     void createDefinedEntry_standardDates_accordingDatesInDateObjects() 
     {
         var appointmentFactory = new AppointmentEntryFactory();
