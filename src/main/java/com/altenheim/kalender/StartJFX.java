af7191f99f115068b659edb0d9f31b0a2e0aedab
LT-6FGD373\Hannes <h.roever@zertificon.com>
2021-05-27 21:51:34 +0200

    fix build path error

--- src/main/java/com/altenheim/kalender/StartJFX.java
+++ src/main/java/com/altenheim/kalender/StartJFX.java
@@ -2,21 +2,19 @@ package com.altenheim.kalender;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-
-import com.altenheim.kalender.controller.MainViewController;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import com.altenheim.kalender.controller.MainViewController;
 
 public class StartJFX extends Application
 {    
     @Override
     public void start(Stage primaryStage) throws Exception 
     {      
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("prototypeUI.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("\\prototypeUI.fxml"));
         var controller = new MainViewController();
         loader.setController(controller);
         Parent root = loader.load();
