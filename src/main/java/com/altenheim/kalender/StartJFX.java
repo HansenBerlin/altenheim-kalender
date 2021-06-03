5f1fa0b3bda975f62ab21617650ebfbf2448a459
LT-6FGD373\Hannes <h.roever@zertificon.com>
2021-06-04 01:32:55 +0200

    test overriding stylesheets

--- src/main/java/com/altenheim/kalender/StartJFX.java
+++ src/main/java/com/altenheim/kalender/StartJFX.java
@@ -1,7 +1,5 @@
 package com.altenheim.kalender;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import javafx.application.Application;
@@ -11,24 +9,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import jfxtras.styles.jmetro.JMetro;
 import jfxtras.styles.jmetro.Style;
-
 import com.altenheim.kalender.controller.MainWindowController;
 
 public class StartJFX extends Application
 {    
     @Override
     public void start(Stage primaryStage) throws Exception 
-    {        
-        var jMetro = new JMetro(Style.LIGHT);
+    {      
         var loader = new FXMLLoader();
-
+        var jMetro = new JMetro(Style.LIGHT);
+        
         loader.setLocation(getClass().getResource("/pocLoadSceneInScene.fxml"));     
         loader.setController(new MainWindowController(primaryStage, jMetro));
-
+        
         Parent root = loader.load();  
         var scene = new Scene(root);
-        
+
         jMetro.setScene(scene);
+        //jMetro.getOverridingStylesheets().getClass().getResource("/rootcolors.css").toExternalForm();
+        
         primaryStage.setScene(scene);            
         primaryStage.setTitle("Smart Planner HWR"); 
         primaryStage.setMaximized(true);
