7b1a41ab0b6a0aa4a90af34348d683588c7724e8
LT-6FGD373\Hannes <h.roever@zertificon.com>
2021-06-09 02:11:14 +0200

    push nico kalender

--- src/main/java/com/altenheim/kalender/Main.java
+++ src/main/java/com/altenheim/kalender/Main.java
@@ -2,11 +2,18 @@ package com.altenheim.kalender;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.text.ParseException;
+
+import com.altenheim.kalender.controller.logicController.ImportExportTest;
+
+import net.fortuna.ical4j.data.ParserException;
 
 public class Main  
 {
-    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException 
+    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException, ParseException, ParserException 
     {
-        StartJFX.main(args);              
+        //StartJFX.main(args);
+        var testimpoerexport = new ImportExportTest();
+        testimpoerexport.testExport();
     }
 }
---
+++ src/main/java/com/altenheim/kalender/controller/logicController/ImportExportTest.java
@@ -0,0 +1,46 @@
+﻿package com.altenheim.kalender.controller.logicController;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.ParseException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import com.calendarfx.model.Entry;
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.property.DtEnd;
+import net.fortuna.ical4j.model.property.DtStart;
+
+
+public class ImportExportTest 
+{
+    public void testExport() throws ParseException, IOException, ParserException
+    {
+        var stream = new FileInputStream("C://Users//Hannes//Documents//test.ics");
+        var builder = new CalendarBuilder();
+        var calendar = builder.build(stream);
+        var components = calendar.getComponents();
+        for (int i = 1; i < components.size(); i++)         
+        {
+            var start = (DtStart)components.get(i).getProperties().get(7);
+            var end = (DtEnd)components.get(i).getProperties().get(8);
+            var startMilli = start.getDate().toInstant().toEpochMilli();
+            var endMilli = end.getDate().toInstant().toEpochMilli();            
+            var entry = createEntryFromMillis(startMilli, endMilli);
+            System.out.println(entry.getStartTime() + " " + entry.getEndTime());
+        }        
+    }  
+
+    private Entry<String> createEntryFromMillis(long start, long end)
+	{
+		var entry = new Entry<String>();
+		var dateStart = LocalDateTime.ofInstant(Instant.ofEpochMilli(start), ZoneId.systemDefault());
+		var dateEnd = LocalDateTime.ofInstant(Instant.ofEpochMilli(end), ZoneId.systemDefault());		
+		entry.changeStartTime(dateStart.toLocalTime());
+		entry.changeStartDate(dateStart.toLocalDate());
+		entry.changeEndTime(dateEnd.toLocalTime());
+		entry.changeEndDate(dateEnd.toLocalDate());
+		return entry;
+	}    
+}
