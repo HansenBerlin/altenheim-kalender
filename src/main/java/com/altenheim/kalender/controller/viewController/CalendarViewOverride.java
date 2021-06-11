10b005d249b8773d769d55d737f87eca2af03fbb
GrandpaNeubert <Robert.Neubert@outlook.com>
2021-06-11 10:45:37 +0200

    add: css calendarfiles dark/ light

--- src/main/java/com/altenheim/kalender/controller/viewController/CalendarViewOverride.java
+++ src/main/java/com/altenheim/kalender/controller/viewController/CalendarViewOverride.java
@@ -6,7 +6,8 @@ public class CalendarViewOverride extends CalendarView
 {
     public CalendarViewOverride()
     {
-        getStylesheets().add(getClass().getResource("/calendar2.css").toExternalForm());
+        //getStylesheets().add(getClass().getResource("/calendarDark.css").toExternalForm());
+        getStylesheets().add(getClass().getResource("/calendarLight.css").toExternalForm());
 
     }
 }
