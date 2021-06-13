90e59804ad6e0a44c311bf508045ef791e7dcfca
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2021-06-13 19:15:26 +0200

    Add all functions findAvailableTimeSlot to Interface

--- src/main/java/com/altenheim/kalender/interfaces/ISmartSearchController.java
+++ src/main/java/com/altenheim/kalender/interfaces/ISmartSearchController.java
@@ -6,5 +6,9 @@ import com.calendarfx.model.Entry;
 public interface ISmartSearchController 
 {
 	public ArrayList<Entry<String>> findAvailableTimeSlot(Entry<String> input, int duration);
-  
+	public ArrayList<Entry<String>> findAvailableTimeSlot(Entry<String> input, int duration, boolean[] weekdays, Entry<String>	selectedHours, int maxNumberOfReturnEntrys);
+	public ArrayList<Entry<String>> findAvailableTimeSlot(Entry<String> input, int duration, boolean[] weekdays, Entry<String>	selectedHours, int timeBefore, int timeAfter, int maxNumberOfReturnEntrys);
+	public ArrayList<Entry<String>> findAvailableTimeSlot(Entry<String> input, int duration, boolean[] weekdays, Entry<String>	selectedHours, ArrayList<ArrayList<Entry<String>>> openingHours, int maxNumberOfReturnEntrys);
+	public ArrayList<Entry<String>> findAvailableTimeSlot(Entry<String> input, int duration, boolean[] weekdays, Entry<String>	selectedHours, ArrayList<ArrayList<Entry<String>>> openingHours, int timeBefore, int timeAfter, int maxNumberOfReturnEntrys);
+	
 }
