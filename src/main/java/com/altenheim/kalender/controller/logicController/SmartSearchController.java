b42b3f71bbd2e20d5cd742eed9a32132523d321c
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2021-06-14 11:42:20 +0200

    Bugfix

--- src/main/java/com/altenheim/kalender/controller/logicController/SmartSearchController.java
+++ src/main/java/com/altenheim/kalender/controller/logicController/SmartSearchController.java
@@ -8,6 +8,7 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.time.DayOfWeek;
 
 import com.calendarfx.model.Calendar;
 import com.calendarfx.model.Entry;
