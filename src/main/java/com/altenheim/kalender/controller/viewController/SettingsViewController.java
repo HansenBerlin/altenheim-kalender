4033837c17bbb38420e428fe7a002d4e586f5eec
CorentinHamp <s_hamp20@stud.hwr-berlin.de>
2021-06-17 20:54:18 +0200

    Remove: unused Methods

--- src/main/java/com/altenheim/kalender/controller/viewController/SettingsViewController.java
+++ src/main/java/com/altenheim/kalender/controller/viewController/SettingsViewController.java
@@ -17,8 +17,7 @@ public class SettingsViewController extends ResponsiveController
 
     @FXML
     void save(ActionEvent event) {
-        String userinput = urlbuilder(Studiengang.getText(),Kurs.getText());
-         settings.seturlvariables(userinput);
+        
     }
     public SettingsViewController(SettingsModel settings)
     {
@@ -31,49 +30,8 @@ public class SettingsViewController extends ResponsiveController
     }    
     
 
-        public String urlbuilder(String inputa, String inputb) {
-            
-            String semester = "";
-            String kurs = "";
-            
-            
-            if (inputb.contains("1")) {
-                 semester = "1";	
-                }
-            else if (inputb.contains("2")) {
-                 semester = "2";	
-                }
-            else if (inputb.contains("3")) {
-                 semester = "3";	
-                }
-            else if (inputb.contains("4")) {
-                 semester = "4";	
-                }
-            else if (inputb.contains("5")) {
-                 semester = "5";	
-                }
-            else if (inputb.contains("6")) {
-                 semester = "6";	
-                }
-            else {
-            System.out.println("Fehler bei Eingabe des Semesters");
-            }
-            
-            if (inputb.contains("a")) {
-                 kurs = "a";	
-                }
-            else if (inputb.contains("b")) {
-                 kurs = "b";	
-            }
-            else if (inputb.contains("c")) {
-                 kurs = "c";	
-                }
-            else {
-                System.out.println("Fehler bei Eingabe des kurses");
-                }
-         return "https://moodle.hwr-berlin.de/fb2-stundenplan/download.php?doctype=.ics&url=./fb2-stundenplaene/"+ inputa + "/semester"+ semester + "/kurs" + kurs;
-         
-        }
+       
+        
         
     
     
