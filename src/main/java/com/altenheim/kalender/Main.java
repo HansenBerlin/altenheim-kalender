aa16cb4fe378e8ca616759c45f52d3b25b4f023c
LT-6FGD373\Hannes <h.roever@zertificon.com>
2021-06-17 19:16:04 +0200

    add class for AES encryption and decryption, test function call in main. flow: userpw-->decrypt hashed pw-->serialize decrypted pwhash-->use decrypted pw hash for decryption of encrypted api key

--- src/main/java/com/altenheim/kalender/Main.java
+++ src/main/java/com/altenheim/kalender/Main.java
@@ -1,5 +1,8 @@
 package com.altenheim.kalender;
 
+import com.altenheim.kalender.controller.logicController.SecureAesController;
+import com.altenheim.kalender.models.SettingsModel;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 
@@ -7,6 +10,12 @@ public class Main
 {
     public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException 
     {       
-        JavaFXLauncher.main(args);              
+        //JavaFXLauncher.main(args);
+
+        var security = new SecureAesController();
+        String userPw = "v;Z3.Z$2M((e,}euJ";
+        var hashedPasswordAfterUserValidation = security.decrypt(userPw, "112233%$§", SettingsModel.PASSWORDHASH);
+        var apiKeyDecrypted = security.decrypt(hashedPasswordAfterUserValidation, "ff&%hf((", SettingsModel.APICYPHER);
+        System.out.println(apiKeyDecrypted);
     }
 }
--- src/main/java/com/altenheim/kalender/models/SettingsModel.java
+++ src/main/java/com/altenheim/kalender/models/SettingsModel.java
@@ -7,20 +7,24 @@ import java.io.Serializable;
 
 public class SettingsModel implements Serializable
 {
+    public final static String APICYPHER = "vzAX1n7COi82yQPOqQa1gA==";
+    public final static String PASSWORDHASH = "zmug2qLvMu11eKqbckKNVs+FjCcmDSK8p3feHqObn/2cwNbtPNG3y8VR5z16Po/zPFPLgjPBYB6muGS8GcBftNqU14PiRl0ho1pq2CovNXYD2TxrMAI3ZANtL375wQbJxAvQZLBXwo6jQ6AxFGe6gA==";
+
     private String pathToSaveBackupFiles = null;
     private PropertyChangeSupport propertyChange = new PropertyChangeSupport(this);
-    public Long scrapingIntervalInMinutes = (long) 2000;     
+    private Long scrapingIntervalInMillis = (long) 2000;
 
-    public long getScrapingInterval() { return scrapingIntervalInMinutes; }
-    public void setScrapingInterval(long interval)
-    {
-        propertyChange.firePropertyChange("scrapingIntervalInMinutes", scrapingIntervalInMinutes, interval);
-        scrapingIntervalInMinutes = interval;
-    }    
-     
-    public void addPropertyChangeListener(PropertyChangeListener listener) 
+
+    public void addPropertyChangeListener(PropertyChangeListener listener)
     {
         propertyChange.addPropertyChangeListener(listener);
+    }
+
+    public long getScrapingInterval() { return scrapingIntervalInMillis; }
+    public void setScrapingInterval(long interval)
+    {
+        propertyChange.firePropertyChange("scrapingIntervalInMillis", scrapingIntervalInMillis, interval);
+        scrapingIntervalInMillis = interval;
     }
 
     public void setCustomPathToSavedFiles() { }
