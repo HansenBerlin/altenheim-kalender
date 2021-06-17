f2a90b4d1836c6c8df42cfe825014ced9dd22ff6
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2021-06-17 20:26:55 +0200

    Refactor and add describtion for the find PossibleTimeSlots function

--- src/main/java/com/altenheim/kalender/controller/logicController/SmartSearchController.java
+++ src/main/java/com/altenheim/kalender/controller/logicController/SmartSearchController.java
@@ -19,22 +19,49 @@ public class SmartSearchController implements ISmartSearchController {
 		this.administrateEntries = administrateEntries;
 	}
 
+	/*Die Methode ist zum Aufruf der Suchlogik aus dem SearchViewController
 
+	Der Methode wird übergeben:
+		input: Der Entry beinhaltet das Startdatum, die Startuhrzeit und die Enduhrzeit für die Terminsuche
+		duration: Hier wird die Länge des gesuchten Termins in Minuten übergeben.
+		weekdays: Hier wird ein boolean Array der Länge 7 übergeben, dabei ist die erste Position Montag und die letzte Sonntag.
+				  (true == Tag darf belegt werden / false == Tag darf nicht belegt werden)
+		openingHours: Hier wird eine Liste der Länge 7 übergeben, die die Wochentage repräsentiert.
+					  Für jeden Wochentag beinhaltet die Liste eine weitere Liste, die alle Öffnungzeiten des Tages (in Entrys mit Start- u. EndTime gespeichert) enthält.
+		timeBefore: Hier wird die Zeit in Minuten übergeben, die vor dem Termin frei sein muss.
+		timeAfter: Hier wird die Zeit in Minuten übergeben, die nach dem Termin frei sein muss.
+		maxNumberOfReturnEntrys: Hier wird die maximale Anzahl an gewünschten Zeiträumen übergeben. 
+								 (Bei Intervallverarbeitung wird hier die Anzahl der Intervalle übergeben)
+		intervalDays: Hier wird die Zeit in Tagen übergeben, die mindestens zwischen zwei Terminen aufeinanderfolgenden Terminen in der Intervallverarbeitung liegen soll.
+					  (Zur Deaktivierung der Intervallsuche wird hier 0 übergeben)
+
+	Die Methode gibt zurück:
+		Liste mit den Zeiträumen für mögliche Einträge
+	*/
 	public ArrayList<Entry<?>> findPossibleTimeSlots(Entry<?> input, int duration, boolean[] weekdays, 
 		ArrayList<ArrayList<Entry<?>>> openingHours, int timeBefore, int timeAfter, int maxNumberOfReturnEntrys, int intervalDays){
-
+		
 		var output = new ArrayList<Entry<?>>(); 
 		var start = input.getStartTime();
 		var end = input.getEndTime();
 		var intervalNumber = 1;
+		LocalDate date = input.getStartDate();
+		var inInterval = true;
 
 		var i = -1;
 		while (output.size()<maxNumberOfReturnEntrys) {
-		i++;
-			var date = input.getStartDate().plusDays(i);
-			if(!weekdays[date.getDayOfWeek().getValue()-1])
-				continue;
+			i++;
+			if (intervalDays==0 || inInterval) { 
+				date = date.plusDays(i);
+				if (intervalDays==0 && inInterval)
+					date = input.getStartDate().plusDays(i);
+			}
 			
+			if(!weekdays[date.getDayOfWeek().getValue()-1]){
+				inInterval = true;
+				continue;
+			}
+				
 			for (var day : openingHours.get(i%7))
 			{
 				var entry = createEntry(date, start, end);
@@ -45,11 +72,13 @@ public class SmartSearchController implements ISmartSearchController {
 				if (end.isAfter(day.getEndTime()))
 					entry.changeEndTime(day.getEndTime());
 				output.addAll(findAvailableTimeSlot(entry, duration, timeBefore, timeAfter));
-				
+	
 				if (intervalDays !=0 && intervalNumber >= output.size()) {
 					reduceListLenght(output, intervalNumber);
 					date = date.plusDays(intervalDays);
 					intervalNumber++;
+					inInterval = false;
+					i = -1;
 				}
 			}			
 		}
@@ -59,25 +88,7 @@ public class SmartSearchController implements ISmartSearchController {
 		return output;
 	}
 
-	private void reduceListLenght(ArrayList<Entry<?>> list, int maxNumberOfEntrys) {
-		while (list.size()>maxNumberOfEntrys) {
-			list.remove(list.size()-1);
-		}
-	}
-
-	private Entry<?> createEntry(LocalDate startAndEnd, LocalTime start, LocalTime end)
-	{
-		var entry = new Entry();				
-		entry.changeStartTime(start);
-		entry.changeEndTime(end);
-		entry.changeStartDate(startAndEnd);
-		entry.changeEndDate(startAndEnd);
-		return entry;
-	}	
-
-
 	public ArrayList<Entry<?>> findAvailableTimeSlot(Entry<?> input, int duration, int before, int after) {			
-		
 		var result = administrateEntries.getSpecificCalendarByIndex(0).findEntries(
 			input.getStartDate(), input.getEndDate(), ZoneId.systemDefault()).values();		
 		var output = new ArrayList<Entry<?>>();
@@ -102,10 +113,21 @@ public class SmartSearchController implements ISmartSearchController {
 				if (checkForDuplicates(output))
 					output.remove(output.size()-1);					
 			}
+		
 		if (result.isEmpty())
 			output.add(input);
-						
+
 		return output;
+	}
+
+	private Entry<?> createEntry(LocalDate startAndEnd, LocalTime start, LocalTime end)
+	{
+		var entry = new Entry();				
+		entry.changeStartTime(start);
+		entry.changeEndTime(end);
+		entry.changeStartDate(startAndEnd);
+		entry.changeEndDate(startAndEnd);
+		return entry;
 	}
 
 	private Entry<?> createEntryFromMillis(long start, long end)
@@ -120,6 +142,12 @@ public class SmartSearchController implements ISmartSearchController {
 		return entry;
 	}
 
+	private void reduceListLenght(ArrayList<Entry<?>> list, int maxNumberOfEntrys) {
+		while (list.size()>maxNumberOfEntrys) {
+			list.remove(list.size()-1);
+		}
+	}
+	
 	private boolean checkForDuplicates(ArrayList<Entry<?>> currentEntries)
 	{
 		if (currentEntries.size() < 2)
