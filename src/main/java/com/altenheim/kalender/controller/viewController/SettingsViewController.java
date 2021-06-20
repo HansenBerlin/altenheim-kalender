22742e0a1af8dc1193b0c5ab474a0681846b36d8
GrandpaNeubert <Robert.Neubert@outlook.com>
2021-06-20 16:46:53 +0200

    SettingsView_unvollständig

--- src/main/java/com/altenheim/kalender/controller/viewController/SettingsViewController.java
+++ src/main/java/com/altenheim/kalender/controller/viewController/SettingsViewController.java
@@ -1,20 +1,90 @@
 package com.altenheim.kalender.controller.viewController;
 
 import com.altenheim.kalender.models.SettingsModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class SettingsViewController extends ResponsiveController
 { 
     private SettingsModel settings;
 
+    @FXML
+    private MenuButton selectionSpecialField, selectionCourse, selectionSemester;
+    @FXML
+    private Button btnImport, btnExport, btnSave;
+    @FXML
+    private TextField inputStreet, inputCity, inputZipCode, inputHouseNumber, inputMail;
+    @FXML
+    private RadioButton adressMailFirst, toolTipsOn, toolTipsOff;
+    @FXML
+    private Text scrappingURL, showStreet, showHouseNumber, showCity, showZipCode, showMail;
+    @FXML
+    private MenuItem selectionSpecialFieldInsurance, selectionSpecialFieldWi;
+    
+    
     public SettingsViewController(SettingsModel settings)
     {
         this.settings = settings;
     }
     
+    @FXML
+    private void initialize ()
+    {
+        inputStreet.setText("Straße");
+        inputHouseNumber.setText(settings.getHouseNumber());
+        inputZipCode.setText(settings.getZipCOde());
+        inputCity.setText(settings.getCity());
+        scrappingURL.setText(settings.getCalendarParser());
+    }
+
+    @FXML
+    void changeSpecialField(ActionEvent event) 
+    {
+        selectionSpecialField.setText(((MenuItem)event.getSource()).getText());
+    }
+
+    @FXML
+    void changeCourse(ActionEvent event) 
+    {
+        selectionCourse.setText(((MenuItem)event.getSource()).getText());
+    }
+
+    @FXML
+    void changeSemester(ActionEvent event) 
+    {
+        selectionSemester.setText(((MenuItem)event.getSource()).getText());
+    }
+    
+    @FXML
+    void saveSettings(ActionEvent event) 
+    {
+        settings.setStreet(inputStreet.getText());
+        showStreet.setText(settings.getStreet());
+
+        settings.setHouseNumber(inputHouseNumber.getText());
+        showHouseNumber.setText(settings.getHouseNumber());
+
+        settings.setZipCode(inputZipCode.getText());
+        showZipCode.setText(settings.getZipCOde());
+
+        settings.setCity(inputCity.getText());
+        showCity.setText(settings.getCity());
+        
+        settings.setMail(inputMail.getText());
+        showMail.setText(settings.getMail());
+        
+        String resultURL = "https://moodle.hwr-berlin.de/fb2-stundenplan/download.php?doctype=.ics&url=./fb2-stundenplaene/"+ selectionSpecialField.getText() + "/semester"+ selectionSemester.getText().charAt(5) + "/kurs" + selectionCourse.getText().replaceFirst("keine Kurse", "");
+        settings.setCalendarParser(resultURL);
+        scrappingURL.setText(settings.getCalendarParser());
+    }
     public void changeContentPosition() 
     {
         
-    }    
+    }
 }
-
-
--- src/main/java/com/altenheim/kalender/models/SettingsModel.java
+++ src/main/java/com/altenheim/kalender/models/SettingsModel.java
@@ -8,7 +8,14 @@ public class SettingsModel
 {
     private String pathToSaveBackupFiles = null;
     private PropertyChangeSupport propertyChange = new PropertyChangeSupport(this);
-    public Long scrapingIntervalInMinutes = (long) 2000;     
+    public Long scrapingIntervalInMinutes = (long) 2000; 
+    private String scrappingURL = "HWR.hallo.com"; 
+    private String street = "Straße";
+    private String houseNumber = "Hausnr.";
+    private String zipCode = "Plz";
+    private String city = "Stadt";
+    private String mail = "E-Mail Adresse";  
+
 
     public long getScrapingInterval() { return scrapingIntervalInMinutes; }
     public void setScrapingInterval(long interval)
@@ -24,4 +31,27 @@ public class SettingsModel
 
     public void setCustomPathToSavedFiles() { }
     public String getCustomPathToSavedFiles() { return pathToSaveBackupFiles; }
+
+    
+    public void setCalendarParser(String scrappingURL) { this.scrappingURL = scrappingURL; }
+    public String getCalendarParser() { return scrappingURL; }
+    
+    public void setStreet (String street) {this.street = street;    }
+    public String getStreet() {return street;}
+    
+    public void setHouseNumber (String houseNumber) {this.houseNumber=houseNumber;}
+    public String getHouseNumber () {return houseNumber;}
+    
+    public void setZipCode (String zipCode) {this.zipCode=zipCode;}
+    public String getZipCOde () {return zipCode;}
+
+    public void setCity (String city) {this.city=city;}
+    public String getCity (){return city;}
+    
+    public void setMail(String mail) { this.mail = mail; }
+    public String getMail() { return mail; }
+
 }
+
+
+
