cd4f8aa696f75451b935d27ff0fb19ed5961d75c
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2021-07-03 11:53:13 +0200

    Add: DateSuggestionController

---
+++ src/main/java/com/altenheim/kalender/controller/logicController/DateSuggestionController.java
@@ -0,0 +1,12 @@
+package com.altenheim.kalender.controller.logicController;
+
+import com.altenheim.kalender.interfaces.IDateSuggestionController;
+import com.calendarfx.model.Entry;
+
+public class DateSuggestionController implements IDateSuggestionController {
+    
+    public Entry<String> getDateSuggestionFromEntryList() {
+        
+        return null;
+    }
+}
