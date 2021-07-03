90e1628282b5f2dedfb34f5ee651c095d5fe3b89
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2021-07-03 16:55:59 +0200

    Add: getDateSuggestionFromEntryList method

--- src/main/java/com/altenheim/kalender/controller/logicController/DateSuggestionController.java
+++ src/main/java/com/altenheim/kalender/controller/logicController/DateSuggestionController.java
@@ -1,12 +1,36 @@
 package com.altenheim.kalender.controller.logicController;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import com.altenheim.kalender.interfaces.IDateSuggestionController;
 import com.calendarfx.model.Entry;
 
 public class DateSuggestionController implements IDateSuggestionController {
-    
-    public Entry<String> getDateSuggestionFromEntryList() {
-        
-        return null;
+
+    public Entry<String> getDateSuggestionFromEntryList(ArrayList<Entry<?>> input, LocalDateTime startSearchDateTime, int dateLenght) {
+        if (input == null || input.isEmpty() || input.get(input.size()).getEndAsLocalDateTime().isBefore(startSearchDateTime.plusMinutes((long) dateLenght + 1))) {
+            return null;
+        }
+
+        var runNumber = 0;
+        while (true) {
+            if (runNumber > input.size()) {
+                return null;
+            }
+
+            if (input.get(runNumber).getStartAsLocalDateTime().isBefore(startSearchDateTime.plusSeconds(1))
+                && input.get(runNumber).getEndAsLocalDateTime().isAfter(startSearchDateTime.plusMinutes(dateLenght).minusSeconds(1))) 
+            {
+                var output = new Entry<String>();
+                output.changeStartDate(startSearchDateTime.toLocalDate());
+                output.changeStartTime(startSearchDateTime.toLocalTime());
+                startSearchDateTime = startSearchDateTime.plusMinutes(dateLenght);
+                output.changeEndDate(startSearchDateTime.toLocalDate());
+                output.changeEndTime(startSearchDateTime.toLocalTime());
+                return output;
+            }
+            runNumber++;
+        }
     }
 }
