59fcdbb741332c5c04bb8326dd150e7ebb25e518
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2021-07-03 17:37:58 +0200

    Add first test for Date SuggestionController

---
+++ src/test/java/com/altenheim/kalender/DateSuggestionControllerTest.java
@@ -0,0 +1,48 @@
+package com.altenheim.kalender;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import com.altenheim.kalender.controller.logicController.DateSuggestionController;
+import com.calendarfx.model.Entry;
+import org.junit.jupiter.api.Test;
+
+public class DateSuggestionControllerTest {
+    @Test
+    void getDateSuggestionFromEntryList_ThreePossibleDays_ShouldReturnDateOnFirstDay()
+    {
+        var entryOne = createEntryDummy(10, 18, 1, 1, 1, 1);
+        var entryTwo = createEntryDummy(10, 18, 2, 2, 1, 1);
+        var entryThree = createEntryDummy(10, 18, 3, 3, 1, 1);
+        var input = new ArrayList<Entry<?>>();
+        input.add(entryOne);
+        input.add(entryTwo);
+        input.add(entryThree);
+
+        var startSearchDateTime = LocalDateTime.of(2021, 1, 1, 10, 0);
+
+        var controller = new DateSuggestionController();
+        var result = controller.getDateSuggestionFromEntryList(input, startSearchDateTime, 60);
+
+        var finalResult = result.getStartAsLocalDateTime().equals(LocalDateTime.of(2021, 1, 1, 10, 0));
+        assertTrue(finalResult);
+        
+    }
+
+
+
+
+    private Entry<?> createEntryDummy(int startTime, int EndTime, int startDay, int endDay, int startMonth, int endMonth)
+    {
+        var entryUser = new Entry<String>("User Preference");
+        var startDate = LocalDate.of(2021, startMonth, startDay);  
+        var endDate = LocalDate.of(2021, endMonth, endDay);  
+        entryUser.changeStartDate(startDate);
+        entryUser.changeEndDate(endDate);
+        entryUser.changeStartTime(LocalTime.of(startTime, 00, 00));
+        entryUser.changeEndTime(LocalTime.of(EndTime, 00, 00));
+        return entryUser;
+    }
+}
