6297032e419300e7cb746b956683bf8ecd7b2178
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2021-07-03 18:37:53 +0200

    Add: getDateSuggestionFromEntryList to Interface

--- src/main/java/com/altenheim/kalender/interfaces/IDateSuggestionController.java
+++ src/main/java/com/altenheim/kalender/interfaces/IDateSuggestionController.java
@@ -1,5 +1,10 @@
 package com.altenheim.kalender.interfaces;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import com.calendarfx.model.Entry;
+
 public interface IDateSuggestionController {
-    
+    public Entry<String> getDateSuggestionFromEntryList(ArrayList<Entry<?>> input, LocalDateTime startSearchDateTime, int dateLenght);
 }
