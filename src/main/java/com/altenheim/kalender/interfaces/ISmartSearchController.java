efb118247838e1fe0dc3a82f78a446040b9b4342
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2021-07-04 19:21:02 +0200

    Refactor

--- src/main/java/com/altenheim/kalender/interfaces/ISmartSearchController.java
+++ src/main/java/com/altenheim/kalender/interfaces/ISmartSearchController.java
@@ -1,11 +1,12 @@
 package com.altenheim.kalender.interfaces;
 
 import java.util.ArrayList;
+import java.util.List;
+
 import com.calendarfx.model.Entry;
 
-public interface ISmartSearchController 
-{
-	ArrayList<Entry<?>> findPossibleTimeSlots(Entry<?> input, int duration, boolean[] weekdays,
-													 ArrayList<ArrayList<Entry<?>>> openingHours, int timeBefore,
-													 int timeAfter, int maxNumberOfReturnEntrys, int intervalDays);
+public interface ISmartSearchController {
+	List<Entry<?>> findPossibleTimeSlots(Entry<?> input, int duration, boolean[] weekdays,
+			ArrayList<ArrayList<Entry<?>>> openingHours, int timeBefore, int timeAfter, int maxNumberOfReturnEntrys,
+			int intervalDays);
 }
