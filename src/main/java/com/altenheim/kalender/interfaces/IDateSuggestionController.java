ba3685fed308b7321f3fb4893b52974fe99ca483
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2021-07-04 19:23:38 +0200

    Delete wrong files

--- src/main/java/com/altenheim/kalender/interfaces/IDateSuggestionController.java
+++
@@ -1,9 +0,0 @@
-package com.altenheim.kalender.interfaces;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import com.calendarfx.model.Entry;
-
-public interface IDateSuggestionController {
-    Entry<String> getDateSuggestionFromEntryList(ArrayList<Entry<?>> input, LocalDateTime startSearchDateTime, int dateLenght);
-}
