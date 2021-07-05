13096ff88a3452f5f901871cadccb6302e421194
GrandpaNeubert <Robert.Neubert@outlook.com>
2021-07-05 21:44:08 +0200

    requested changes

--- src/main/java/com/altenheim/kalender/models/SettingsModel.java
+++ src/main/java/com/altenheim/kalender/models/SettingsModel.java
@@ -16,7 +16,7 @@ public class SettingsModel
     private SimpleStringProperty city = new SimpleStringProperty();
     private SimpleStringProperty mail = new SimpleStringProperty();  
 
-    public long getScrapingInterval() { return scrapingIntervalInMillis; }
+    public long getScrapingInterval() {return scrapingIntervalInMillis;}
     public void setScrapingInterval(long interval)
     {
         propertyChange.firePropertyChange("scrapingIntervalInMinutes", scrapingIntervalInMillis, interval);
@@ -38,13 +38,13 @@ public class SettingsModel
     public SimpleStringProperty getStreet() {return street;}
     
     public void setHouseNumber (String houseNumber) {this.houseNumber.set(houseNumber);}
-    public SimpleStringProperty getHouseNumber () {return houseNumber;}
+    public SimpleStringProperty getHouseNumber() {return houseNumber;}
     
     public void setZipCode (String zipCode) {this.zipCode.set(zipCode);}
-    public SimpleStringProperty getZipCOde () {return zipCode;}
+    public SimpleStringProperty getZipCOde() {return zipCode;}
 
     public void setCity (String city) {this.city.set(city);}
-    public SimpleStringProperty getCity (){return city;}
+    public SimpleStringProperty getCity() {return city;}
     
     public void setMail(String mail) { this.mail.set(mail);}
     public SimpleStringProperty getMail() {return mail;}
